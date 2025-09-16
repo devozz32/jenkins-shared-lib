4f5c2ae804210cad240ae1424d136c827bb0c53b
devozz32 <talkoragi25@gmail.com>
2025-09-16 15:23:57 +0300

    Update and rename gitpull to gitpull.groovy

--- vars/gitpull.groovy
+++ vars/gitpull.groovy
@@ -1,4 +1,4 @@
-def gitPull(String repoUrl, String branchName) {
+def call(String repoUrl, String branchName) {
     println "git pull from:${repoUrl}, branch: ${branchName}"
     def pullCmd = ["git", "-C", ".", "pull", repoUrl, branchName]
     def pullProc = pullCmd.execute()
