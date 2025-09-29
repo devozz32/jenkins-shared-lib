0c21017feacd83c694502e39ea564b2221cdfe61
devozz32 <talkoragi25@gmail.com>
2025-09-29 15:17:05 +0300

    Update snykScan.groovy

--- vars/snykScan.groovy
+++ vars/snykScan.groovy
@@ -9,12 +9,13 @@ def call(Map args = [:]) {
         set -e
         EXIT_CODE=0
         for svc in ${services.join(' ')}; do
+            SAFE_NAME=\$(echo \$svc | sed 's#[/:]#_#g')
             echo "Scanning container for \$svc"
-            snyk container test \$svc --json > \${svc}-snyk.json || true
+            snyk container test \$svc --json > \${SAFE_NAME}-snyk.json || true
 
-            CRITICAL=\$(jq '[.vulnerabilities[] | select(.severity=="critical")] | length' \${svc}-snyk.json)
-            HIGH=\$(jq '[.vulnerabilities[] | select(.severity=="high")] | length' \${svc}-snyk.json)
-            MED=\$(jq '[.vulnerabilities[] | select(.severity=="medium")] | length' \${svc}-snyk.json)
+            CRITICAL=\$(jq '[.vulnerabilities[] | select(.severity=="critical")] | length' \${SAFE_NAME}-snyk.json)
+            HIGH=\$(jq '[.vulnerabilities[] | select(.severity=="high")] | length' \${SAFE_NAME}-snyk.json)
+            MED=\$(jq '[.vulnerabilities[] | select(.severity=="medium")] | length' \${SAFE_NAME}-snyk.json)
 
             echo "Results for \$svc:"
             if [ "\$CRITICAL" -gt 0 ] || [ "\$HIGH" -gt 0 ]; then
